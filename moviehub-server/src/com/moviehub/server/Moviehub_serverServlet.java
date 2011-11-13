e4e8fbd67968224576871be5f6d17210e9259271
Mikael Åhlén <mikael.ahlen@gmail.com>
2011-11-13 18:50:33 +0100

    added initial server/api project

---
+++ moviehub-server/src/com/moviehub/server/Moviehub_serverServlet.java
@@ -0,0 +1,13 @@
+package com.moviehub.server;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class Moviehub_serverServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
