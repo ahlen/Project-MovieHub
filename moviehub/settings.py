1b8eae755393d7f919ce05c9bf309a456ac0f4a7
Mikael Åhlén <mikael.ahlen@gmail.com>
2011-12-03 21:26:00 +0100

    Update moviehub/settings.py

--- moviehub/settings.py
+++ moviehub/settings.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-GOOGLE_OAUTH2_ID = "79021744695.apps.googleusercontent.com"
-GOOGLE_OAUTH2_SECRET= "pLds0PEpc0Y1QXCnnrE8UciN"
+GOOGLE_OAUTH2_ID = ""
+GOOGLE_OAUTH2_SECRET= ""
 
-TMDB_API_KEY = "a314e35b02e9cad181b1f37c96989b95"
+TMDB_API_KEY = ""
